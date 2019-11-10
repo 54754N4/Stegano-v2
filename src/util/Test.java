7a9a8c0cb0d52e8663117320c462434e8f7d4860
54754N4 <35555527+54754N4@users.noreply.github.com>
2019-11-10 14:40:04 +0700

    Added global debugging helper methods

--- src/util/Test.java
+++ src/util/Test.java
@@ -1,8 +1,27 @@
 package util;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Test {
 	public static boolean allUnder(int i, byte[] bytes) {
 		for (byte b : bytes) if (b > i) return false;
 		return true;
+	}
+	
+	public static void test(File file) throws FileNotFoundException, IOException {
+		try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file))) {
+			System.out.println(bis.available());
+			while (bis.available()>0) {
+				int ib = bis.read();
+				byte b = (byte) ib;
+				if (ib < 0 || ib > 127) System.out.println("int: "+ib);
+				if (b < 0) System.out.println("byte: "+b);
+			}
+			System.out.println("Finished "+file);
+		}
 	}
 }
