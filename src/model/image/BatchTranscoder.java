5610e45256bb419a495bb72a3ba8db085d5fd049
54754N4 <35555527+54754N4@users.noreply.github.com>
2020-01-18 18:23:44 +0700

    Made pixel processing on-the-fly

--- src/model/image/BatchTranscoder.java
+++ src/model/image/BatchTranscoder.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.IOException;
 
 import util.Conversions;
+import util.FastRGB;
 
 public abstract class BatchTranscoder extends PixelTranscoder {
 	
@@ -17,24 +18,28 @@ public abstract class BatchTranscoder extends PixelTranscoder {
 	
 	@Override
 	public void write(BufferedImage bimage, byte[] bytes) throws Exception {
-//		System.out.println("BEFORE="+new HashedFile(bytes).hexChecksum());
-		Pixel[] pixels = extractPixels(bimage);			// speed up by making it only extract and write payload.size pixels 
+		FastRGB frgb = new FastRGB(bimage);
 		int c = 0;	//written bytes count
 		byte[][] packed = Conversions.pack(bytes);
 		for (int x = 0; x < bimage.getWidth(); x++) 
             for (int y = 0; y < bimage.getHeight(); y++) 
             	if (c < packed.length)
-            		bimage.setRGB(x, y, batchHide(pixels[c], packed[c++]).toARGB());
+            		bimage.setRGB(x, y, batchHide(new Pixel(frgb.getRGB(x, y)), packed[c++]).toARGB());
 		System.out.println(String.format("Affected %d/%d pixels.", c, bimage.getWidth()*bimage.getHeight()));
 	}
 	
 	@Override
 	public byte[] extractHidden(File image) throws IOException {
 		BufferedImage bimage = getWriteableImage(image);	
-		Pixel[] pixels = extractPixels(bimage);
-		byte[][] bytes = new byte[pixels.length][];
-		int c=0;
-		for (Pixel pixel : pixels) bytes[c++] = batchUnhide(pixel);
+		FastRGB frgb = new FastRGB(bimage);
+		int total = bimage.getHeight()*bimage.getWidth();
+		byte[][] bytes = new byte[total][];
+		for (int i=0; i<total; i++) 
+			bytes[i] = batchUnhide(
+				new Pixel(
+					frgb.getRGB(
+						i/bimage.getHeight(), 
+						i%bimage.getHeight())));
 		return Conversions.flatten(bytes);
 	}
 	
--- src/model/image/PixelTranscoder.java
+++ src/model/image/PixelTranscoder.java
@@ -1,6 +1,5 @@
 package model.image;
 
-import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -21,23 +20,27 @@ public abstract class PixelTranscoder extends Metafier {
 	protected abstract Pixel hide(Pixel pixel, byte b) throws Exception;
 	protected abstract byte unhide(Pixel pixel);
 	
-	public void write(BufferedImage bimage, byte[] bytes) throws Exception {
-//		System.out.println("BEFORE="+new HashedFile(bytes).hexChecksum());
-		Pixel[] pixels = extractPixels(bimage);			// speed up by making it only extract and write payload.size pixels 
+	public void write(BufferedImage bimage, byte[] bytes) throws Exception { 
+		FastRGB frgb = new FastRGB(bimage);
 		int c = 0;	//written bytes count
 		for (int x = 0; x < bimage.getWidth(); x++) 
             for (int y = 0; y < bimage.getHeight(); y++) 
             	if (c < bytes.length)
-            		bimage.setRGB(x, y, hide(pixels[c], bytes[c++]).toARGB());
+            		bimage.setRGB(x, y, hide(new Pixel(frgb.getRGB(x, y)), bytes[c++]).toARGB());
 		System.out.println(String.format("Affected %d/%d pixels.", c, bimage.getWidth()*bimage.getHeight()));
 	}
 	
 	public byte[] extractHidden(File image) throws IOException {
 		BufferedImage bimage = getWriteableImage(image);	
-		Pixel[] pixels = extractPixels(bimage);
-		byte[] bytes = new byte[pixels.length];
-		int c=0;
-		for (Pixel pixel : pixels) bytes[c++] = unhide(pixel);
+		FastRGB frgb = new FastRGB(bimage);
+		int total = bimage.getHeight()*bimage.getWidth();
+		byte[] bytes = new byte[total];
+		for (int i=0; i<total; i++) 
+			bytes[i] = unhide(
+				new Pixel(
+					frgb.getRGB(
+						i/bimage.getHeight(), 
+						i%bimage.getHeight())));
 		return bytes;
 	}
 	
@@ -51,16 +54,6 @@ public abstract class PixelTranscoder extends Metafier {
 			bimage.getWidth(), 
 			bimage.getHeight(), 
 			bimage.getWidth()/(double) bimage.getHeight()));
-	}
-	
-	public static Pixel[] extractPixels(BufferedImage bimage) throws IOException {
-		FastRGB frgb = new FastRGB(bimage);
-		Pixel[] colours = new Pixel[bimage.getWidth()*bimage.getHeight()];
-		int count = 0;
-		for (int x = 0; x < bimage.getWidth(); x++)
-            for (int y = 0; y < bimage.getHeight(); y++)
-            	colours[count++] = new Pixel(new Color(frgb.getRGB(x, y), true));
-        return colours;
 	}
 	
 	public static BufferedImage getWriteableImage(File image) throws IOException {		//https://www.codeproject.com/Questions/542826/getRBGplusdoesn-tplusreturnplusvalueplussetplusb
