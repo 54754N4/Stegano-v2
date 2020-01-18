817181afa1ef5eabaa2406a76a45b3c238e89cf2
54754N4 <35555527+54754N4@users.noreply.github.com>
2020-01-18 15:16:27 +0700

    Added tests and changes for new techniques and refactorings

---
+++ src/test/TestImages.java
@@ -0,0 +1,52 @@
+package test;
+
+import java.io.File;
+
+import image.AlphaTranscoder;
+import image.BatchLSBTranscoder;
+import image.LSBTranscoder;
+import image.Pixel.Target;
+import model.ImageStegano;
+import model.ParsedResults;
+
+//check dickbaldwin.com
+public class TestImages {
+	private static final File image = new File("bigimagepf.bmp"),						// image to hide into
+		payload = new File("aFile.txt");							// file to hide
+	
+	public static void main(String[] args) throws Exception {
+		testAlpha();
+		System.out.println();
+		testLSB();
+		System.out.println();
+//		testLSB2();
+	}
+	
+	public static void testAlpha() throws Exception {
+		ImageStegano is = new ImageStegano(new AlphaTranscoder("#", 2));		// separator-> metadata + payload 
+		File carrier = is.hide(payload, image, "newImage.png","png"); 
+		System.out.println("Carrier="+carrier.getName());			
+		
+		ParsedResults extracted = is.extractFile(carrier);				// extracts data from carrier file
+		System.out.println("Hidden="+extracted.file.getName()); 		// Also contains other parsed results
+	}
+	
+	public static void testLSB() throws Exception {
+		ImageStegano is = new ImageStegano(new LSBTranscoder("#", 2, Target.BLUE));		// separator-> metadata + payload 
+		File carrier = is.hide(payload, image, "newImage.png","png"); 
+		System.out.println("Carrier="+carrier.getName());			
+		
+		ParsedResults extracted = is.extractFile(carrier);				// extracts data from carrier file
+		System.out.println("Hidden="+extracted.file.getName()); 		// Also contains other parsed results
+	}
+	
+	public static void testLSB2() throws Exception {
+		ImageStegano is = new ImageStegano(new BatchLSBTranscoder("#", 2));		// separator-> metadata + payload 
+		File carrier = is.hide(payload, image, "newImage.png","png"); 
+		System.out.println("Carrier="+carrier.getName());			
+		
+		ParsedResults extracted = is.extractFile(carrier);				// extracts data from carrier file
+		System.out.println("Hidden="+extracted.file.getName()); 		// Also contains other parsed results
+	}
+
+}
