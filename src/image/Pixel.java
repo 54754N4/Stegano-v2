9bb2e8de60ed104535eb27ce6d419f77b043a3b0
54754N4 <35555527+54754N4@users.noreply.github.com>
2020-01-18 15:15:32 +0700

    Generified pixel transcoding

--- src/image/Pixel.java
+++ src/image/Pixel.java
@@ -3,10 +3,10 @@ package image;
 import java.awt.Color;
 import java.util.Arrays;
 
-import image.errors.ImageAlreadyTransparentException;
-import util.BitsUtil;
+import image.error.ImageAlreadyTransparentException;
 
 public class Pixel {
+	public static enum Target { ALPHA, RED, GREEN, BLUE };
 	private Color colour;
 	private int alpha, red, green, blue;
 	
@@ -21,12 +21,10 @@ public class Pixel {
 		red = colour.getRed();			// ((ARGB & 0x00ff0000) >> 16);
 		green = colour.getGreen();		// ((ARGB & 0x0000ff00) >> 8);
 		blue = colour.getBlue();		// (ARGB & 0x000000ff);
-		if (ARGB != toARGB()) 
-			System.out.println("WTF");	// should never print but just in cased we messed up
 	}
 	
 	public int[] getComponents() {
-		return new int[] {alpha,red,green,blue};
+		return new int[] {alpha, red, green, blue};
 	}
 	
 	public int getAlpha() {
@@ -45,60 +43,33 @@ public class Pixel {
 		return blue;
 	}
 	
+	public int get(Target t) {
+		switch (t) {
+			case ALPHA:	return getAlpha();
+			case BLUE: return getBlue();
+			case GREEN: return getGreen();
+			case RED: return getRed();
+			default: throw new IllegalArgumentException("Invalid target");
+		}
+	}
+	
+	public Pixel set(Target t, int value) {
+		switch (t) {
+			case ALPHA:	alpha = value; break;
+			case BLUE: blue = value; break;
+			case GREEN: green = value; break;
+			case RED: red = value; break;
+			default: throw new IllegalArgumentException("Invalid target");
+		}
+		return this;
+	}
+	
 	public boolean notWhite() {
 		return !(alpha == red && red == green && green == blue && blue == 255);
 	}
 	
-	public Pixel hideLSB(byte halfByte) {
-		if (halfByte > 0xF || halfByte < 0) throw new IllegalArgumentException("Invalid half byte : "+halfByte);
-		for (int i=0; i<4; i++) {
-			int bit = BitsUtil.getBit(halfByte, i);
-			switch (i) {
-				case 0: alpha = BitsUtil.toggleBit(alpha, bit, 0); break;
-				case 1: red = BitsUtil.toggleBit(red, bit, 0); break;
-				case 2: green = BitsUtil.toggleBit(green, bit, 0); break;
-				default: blue = BitsUtil.toggleBit(blue, bit, 0); break;
-			}
-		}
-		return this;
-	}
-	
-	public byte unhideLSB() {
-		int[] component = getComponents();
-		int halfByte = 0, bit;
-		for (int i=0; i<4; i++) {
-			bit = BitsUtil.getBit(component[i], 0);
-			halfByte = BitsUtil.toggleBit(halfByte, bit, i);
-		}
-		return (byte) halfByte;
-	}
-
-	public Pixel hideLSB2(byte b) {
-		for (int i=0; i<4; i++) {
-			int bits = BitsUtil.getBits(b, i*2, (1+i)*2);
-			switch (i) {
-				case 0: alpha = BitsUtil.copy(alpha, bits, 2); break;
-				case 1: red = BitsUtil.copy(red, bits, 2); break;
-				case 2: green = BitsUtil.copy(green, bits, 2); break;
-				default: blue = BitsUtil.copy(blue, bits, 2); break;
-			}
-		}
-		return this;
-	}
-	
-	public byte unhideLSB2() {
-		int[] component = getComponents();
-		int aByte = 0, bits;
-		for (int i=0; i<4; i++) {
-			bits = BitsUtil.getBits(component[i], 2);
-			aByte += bits << i*2;
-		}
-		return (byte) aByte;
-	}
-	
 	public Pixel hideAlpha(int aByte) throws ImageAlreadyTransparentException {
-		if (alpha != 255) throw new ImageAlreadyTransparentException();
-		alpha -= aByte;
+		alpha = 255 - aByte;
 		return this;
 	}
 	
@@ -109,11 +80,17 @@ public class Pixel {
 	}
 	
 	public int toARGB() {
-		return (alpha << 24) | (red << 16) | (green << 8) | blue;
+		return (alpha << 24) 
+			| (red << 16) 
+			| (green << 8) 
+			| blue;
 	}
 	
 	public boolean equals(Pixel p) {
-		return alpha == p.alpha && red == p.red && green == p.green && blue == p.blue; 
+		return alpha == p.alpha 
+			&& red == p.red 
+			&& green == p.green 
+			&& blue == p.blue; 
 	}
 	
 	public String toBinaryString() {
@@ -126,13 +103,13 @@ public class Pixel {
 		return Arrays.toString(getComponents());
 	}
 	
-	public static void main(String[] args) {
-		Pixel p;
-		byte b = (byte) 0;
-		while (++b != 0) {
-			p = new Pixel(Color.WHITE);
-			if (p.hideLSB2(b).unhideLSB2() != b)
-				System.out.println(p.toBinaryString());
-		}
-	}
+//	public static void main(String[] args) {
+//		Pixel p;
+//		byte b = (byte) 0;
+//		while (++b != 0) {
+//			p = new Pixel(Color.WHITE);
+//			if (p.hideLSB(b).unhideLSB() != b)
+//				System.out.println(p.toBinaryString());
+//		}
+//	}
 }
---
+++ src/image/PixelTranscoder.java
@@ -0,0 +1,75 @@
+package image;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import image.error.ImageWriterNotFoundException;
+import image.error.SmallResolutionException;
+import model.Metafier;
+import util.FastRGB;
+
+public abstract class PixelTranscoder extends Metafier {
+	
+	public PixelTranscoder(String sep, int subdivisions) {
+		super(sep, subdivisions);
+	}
+	
+	protected abstract Pixel hide(Pixel pixel, byte b) throws Exception;
+	protected abstract byte unhide(Pixel pixel);
+	
+	public void write(BufferedImage bimage, byte[] bytes) throws Exception {
+//		System.out.println("BEFORE="+new HashedFile(bytes).hexChecksum());
+		Pixel[] pixels = extractPixels(bimage);			// speed up by making it only extract and write payload.size pixels 
+		int c = 0;	//written bytes count
+		for (int x = 0; x < bimage.getWidth(); x++) 
+            for (int y = 0; y < bimage.getHeight(); y++) 
+            	if (c < bytes.length)
+            		bimage.setRGB(x, y, hide(pixels[c], bytes[c++]).toARGB());
+		System.out.println(String.format("Affected %d/%d pixels.", c, bimage.getWidth()*bimage.getHeight()));
+	}
+	
+	public byte[] extractHidden(File image) throws IOException {
+		BufferedImage bimage = getWriteableImage(image);	
+		Pixel[] pixels = extractPixels(bimage);
+		byte[] bytes = new byte[pixels.length];
+		int c=0;
+		for (Pixel pixel : pixels) bytes[c++] = unhide(pixel);
+		return bytes;
+	}
+	
+	public void verifySize(BufferedImage bimage, byte[] bytes) throws SmallResolutionException {
+		int totalPixels = bimage.getWidth() * bimage.getHeight();
+		if (bytes.length > totalPixels)
+			throw new SmallResolutionException(bytes.length, totalPixels);
+		System.out.println("Total bytes/pixels = "+bytes.length+"/"+totalPixels);
+		System.out.println(String.format("Image width/height/ratio = %d/%d/%f",bimage.getWidth(), bimage.getHeight(), bimage.getWidth()/(double) bimage.getHeight()));
+	}
+	
+	public static Pixel[] extractPixels(BufferedImage bimage) throws IOException {
+		FastRGB frgb = new FastRGB(bimage);
+		Pixel[] colours = new Pixel[bimage.getWidth()*bimage.getHeight()];
+		int count = 0;
+		for (int x = 0; x < bimage.getWidth(); x++)
+            for (int y = 0; y < bimage.getHeight(); y++)
+            	colours[count++] = new Pixel(new Color(frgb.getRGB(x, y), true));
+        return colours;
+	}
+	
+	public static BufferedImage getWriteableImage(File image) throws IOException {		//https://www.codeproject.com/Questions/542826/getRBGplusdoesn-tplusreturnplusvalueplussetplusb
+		BufferedImage src = ImageIO.read(image),
+			writeable = new BufferedImage(src.getWidth(), src.getHeight(), BufferedImage.TYPE_4BYTE_ABGR);
+		writeable.getGraphics().drawImage(src, 0, 0, null);	//null cause not using an ImageObserver
+		return writeable;
+	}
+	
+	public File save(BufferedImage bimage, String name, String format) throws IOException {
+		File out = new File(name);
+		if (!ImageIO.write(bimage, format, out))
+			throw new ImageWriterNotFoundException(format);
+		return out;
+	}
+}
